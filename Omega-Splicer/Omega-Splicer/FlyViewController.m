c901a5a6a4574b6dda060093eafb505c39bf059a
Charles-Adrien Fournier <Charladfr@icloud.com>
2015-09-09 17:19:37 +0200

    Create viewControllers

---
+++ Omega-Splicer/Omega-Splicer/FlyViewController.m
@@ -0,0 +1,37 @@
+//
+//  FlyViewController.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 09/09/15.
+//  Copyright (c) 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import "FlyViewController.h"
+
+@interface FlyViewController ()
+
+@end
+
+@implementation FlyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
