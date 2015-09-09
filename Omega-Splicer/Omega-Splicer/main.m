e3e78b48768f086f78ce4f67f0dd569759aa28f9
Charles-Adrien Fournier <Charladfr@icloud.com>
2015-09-09 14:01:33 +0200

    Initial project

---
+++ Omega-Splicer/Omega-Splicer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 09/09/15.
+//  Copyright (c) 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
