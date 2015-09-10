25e7ad31bba4afa46d001e59e370d50e79d5e408
Charles-Adrien Fournier <Charladfr@icloud.com>
2015-09-10 21:41:36 +0200

    Apple Watch Applications

---
+++ Omega-Splicer/Omega-Splicer WatchKit 1 Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Omega-Splicer WatchKit 1 Extension
+//
+//  Created by Charles-Adrien Fournier on 10/09/15.
+//  Copyright © 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Omega-Splicer/Omega-Splicer WatchKit 1 Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Omega-Splicer WatchKit 1 Extension
+//
+//  Created by Charles-Adrien Fournier on 10/09/15.
+//  Copyright © 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
