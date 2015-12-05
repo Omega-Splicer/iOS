9bfb0e22d36264c7a677b71e4d1bee3be1cd741f
Charles-Adrien Fournier <Charladfr@icloud.com>
2015-12-05 09:43:31 +0300

    Rename OSFlyViewController to OSFlyLandscapeViewController

---
+++ Omega-Splicer/Omega-Splicer/Controller/OSFlyLandscapeViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlyViewController.h
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 09/09/15.
+//  Copyright (c) 2015 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSFlyLandscapeViewController : UIViewController
+
+@end
