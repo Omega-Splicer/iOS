bfd670b2025218fa8812251fa887316752c48a1e
Charles-Adrien Fournier <Charladfr@icloud.com>
2015-12-14 15:42:03 +0300

    xCode files update

--- Omega-Splicer/Omega-SplicerTests/Omega_SplicerTests.m
+++ Omega-Splicer/Omega-SplicerTests/Omega_SplicerTests.m
@@ -9,7 +9,23 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "OSHomeViewController.h"
+#import "OSPairViewController.h"
+#import "OSSettingsViewController.h"
+#import "OSFlyLandscapeViewController.h"
+#import "OSFlyPortraitViewController.h"
+
 @interface Omega_SplicerTests : XCTestCase
+
+@property (nonatomic, strong) OSHomeViewController *homeVC;
+
+@property (nonatomic, strong) OSPairViewController *pairVC;
+
+@property (nonatomic, strong) OSSettingsViewController *settingsVC;
+
+@property (nonatomic, strong) OSFlyLandscapeViewController *flyLandscapeVC;
+
+@property (nonatomic, strong) OSFlyPortraitViewController *flyPortraitVC;
 
 @end
 
@@ -18,6 +34,12 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.homeVC = [[OSHomeViewController alloc] init];
+    self.pairVC = [[OSPairViewController alloc] init];
+    self.settingsVC = [[OSSettingsViewController alloc] init];
+    self.flyLandscapeVC = [[OSFlyLandscapeViewController alloc] init];
+    self.flyPortraitVC = [[OSFlyPortraitViewController alloc] init];
 }
 
 - (void)tearDown {
@@ -25,20 +47,41 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+//- (void)testExample {
+//    // This is an example of a functional test case.
+//    XCTAssert(YES, @"Pass");
+//}
+
+- (void)testHomeView {
+    XCTAssertNotNil(self.homeVC, @"Home view controller not nil");
 }
+
+- (void)testPairView {
+    XCTAssertNotNil(self.pairVC, @"Pair view controller not nil");
+}
+
+- (void)testSettingsView {
+    XCTAssertNotNil(self.settingsVC, @"Settings view controller not nil");
+}
+
+- (void)testFlyLandscapeView {
+    XCTAssertNotNil(self.flyLandscapeVC, @"Fly Landscape view controller not nil");
+}
+
+- (void)testFlyPortraitView {
+    XCTAssertNotNil(self.flyPortraitVC, @"Fly portrait view controller not nil");
+}
+
 
 //- (void)testBad {
 //    XCTAssert(NO, @"DON'T PASS");
 //}
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
 
 @end
