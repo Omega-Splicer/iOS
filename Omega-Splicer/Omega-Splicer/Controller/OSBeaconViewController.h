b553e2b6725a5f3aaac371a2ba89bcf25cedf472
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-02-17 18:40:03 +0300

    Support now Estimote Beacons

---
+++ Omega-Splicer/Omega-Splicer/Controller/OSBeaconViewController.h
@@ -0,0 +1,13 @@
+//
+//  OSBeaconViewController.h
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 02/02/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSBeaconViewController : UIViewController
+
+@end
---
+++ Omega-Splicer/Omega-Splicer/Controller/OSBeaconViewController.m
@@ -0,0 +1,41 @@
+//
+//  OSBeaconViewController.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 02/02/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <EstimoteSDK/EstimoteSDK.h>
+#import "OSBeaconViewController.h"
+
+@interface OSBeaconViewController () <ESTBeaconManagerDelegate>
+
+@property (nonatomic) ESTBeaconManager *beaconManager;
+
+@end
+
+@implementation OSBeaconViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.beaconManager = [ESTBeaconManager new];
+    self.beaconManager.delegate = self;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self.beaconManager requestAlwaysAuthorization];
+    [self.beaconManager startMonitoringForRegion:[[CLBeaconRegion alloc] initWithProximityUUID:ESTIMOTE_PROXIMITY_UUID identifier:@"EstimoteSampleRegion"]];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)dismissBeaconView:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
