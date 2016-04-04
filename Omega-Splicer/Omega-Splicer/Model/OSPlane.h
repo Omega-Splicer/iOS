2698c61e3ee6d4cd6d01097a16a8b998db171050
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 15:03:13 +0300

    documentation

--- Omega-Splicer/Omega-Splicer/Model/OSPlane.h
+++ Omega-Splicer/Omega-Splicer/Model/OSPlane.h
@@ -10,22 +10,53 @@
 
 @interface OSPlane : NSObject
 
+/**
+ *  Plane name
+ */
 @property (nonatomic, strong) NSString *name;
 
+/**
+ *  Plane battery level
+ */
 @property (nonatomic) float battery;
 
+/**
+ *  Plane signal level
+ */
 @property (nonatomic) float signal;
 
+/**
+ *  Plane motor power
+ */
 @property (nonatomic) float motor;
 
+/**
+ *  Initialize an empty plane
+ */
 - (void)initEmptyPlane;
 
+/**
+ *  Initialize a plane with specific name, battery and signal
+ *
+ *  @param name    expected name of the plane
+ *  @param battery expected battery level of the plane
+ *  @param signal  expected signal level of the plane
+ */
 - (void)initPlaneWithName:(NSString *)name battery:(float)battery signal:(float)signal;
 
+/**
+ *  Create example plane 1
+ */
 - (void)planeExample1;
 
+/**
+ *  Create example plane 2
+ */
 - (void)planeExample2;
 
+/**
+ *  Create example plane 3
+ */
 - (void)planeExample3;
 
 @end
