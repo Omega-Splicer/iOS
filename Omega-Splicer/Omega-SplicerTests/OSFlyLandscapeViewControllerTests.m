ebb159c87fb48905d4480d5348f04ba78f6cf13b
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 16:34:48 +0300

    OSFlyLandscapeViewController unit tests

---
+++ Omega-Splicer/Omega-SplicerTests/OSFlyLandscapeViewControllerTests.m
@@ -0,0 +1,33 @@
+//
+//  OSFlyLandscapeViewControllerTests.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 04/04/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "OSFlyLandscapeViewController.h"
+
+@interface OSFlyLandscapeViewControllerTests : XCTestCase
+
+@property (strong, nonatomic) OSFlyLandscapeViewController *flyLandscapeViewController;
+
+@end
+
+@implementation OSFlyLandscapeViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    self.flyLandscapeViewController = [[OSFlyLandscapeViewController alloc] init];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testFlyLandscapeViewController {
+    XCTAssertNotNil(self.flyLandscapeViewController, @"Fly landscape view controller is nil");
+}
+
+@end
