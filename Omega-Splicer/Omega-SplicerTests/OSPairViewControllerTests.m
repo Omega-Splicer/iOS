e2fe193e89494b65dec1f23dceab9664c9d09db1
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 16:19:44 +0300

    OSPairViewController unit test

---
+++ Omega-Splicer/Omega-SplicerTests/OSPairViewControllerTests.m
@@ -0,0 +1,33 @@
+//
+//  OSPairViewControllerTests.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 04/04/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "OSPairViewController.h"
+
+@interface OSPairViewControllerTests : XCTestCase
+
+@property (strong, nonatomic) OSPairViewController *pairViewController;
+
+@end
+
+@implementation OSPairViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    self.pairViewController = [[OSPairViewController alloc] init];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testPairViewController {
+    XCTAssertNotNil(self.pairViewController, @"Pair view controller is nil");
+}
+
+@end
