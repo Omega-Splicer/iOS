8acd17cca422b604a57513b9efa7826fe4246d27
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 16:34:21 +0300

    OSSettingsViewController unit tests

---
+++ Omega-Splicer/Omega-SplicerTests/OSSettingsViewControllerTests.m
@@ -0,0 +1,33 @@
+//
+//  OSSettingsViewControllerTests.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 04/04/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "OSSettingsViewController.h"
+
+@interface OSSettingsViewControllerTests : XCTestCase
+
+@property (strong, nonatomic) OSSettingsViewController *settingsViewController;
+
+@end
+
+@implementation OSSettingsViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    self.settingsViewController = [[OSSettingsViewController alloc] init];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testSettingsViewController {
+    XCTAssertNotNil(self.settingsViewController, @"Setting view controller is nil");
+}
+
+@end
