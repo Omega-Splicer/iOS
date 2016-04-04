001ec618a084ee4f072a5f86d8cae617a707be09
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 16:19:33 +0300

    OSHomeViewController unit test

---
+++ Omega-Splicer/Omega-SplicerTests/OSHomeViewControllerTests.m
@@ -0,0 +1,33 @@
+//
+//  OSHomeViewControllerTests.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 04/04/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "OSHomeViewController.h"
+
+@interface OSHomeViewControllerTests : XCTestCase
+
+@property (strong, nonatomic) OSHomeViewController *homeViewController;
+
+@end
+
+@implementation OSHomeViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    self.homeViewController = [[OSHomeViewController alloc] init];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testHomeViewController {
+    XCTAssertNotNil(self.homeViewController, @"Home view controller is nil");
+}
+
+@end
