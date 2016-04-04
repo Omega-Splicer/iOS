aaba17518e931c8af4a120fdc34e49d648ebf3b8
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 14:27:39 +0300

    Documentation

--- Omega-Splicer/Omega-Splicer/Components/CFSliderView.h
+++ Omega-Splicer/Omega-Splicer/Components/CFSliderView.h
@@ -10,16 +10,44 @@
 
 @interface CFSliderView : UIView
 
+/**
+ *  Method used to build the slider with default value
+ */
 - (void)buildSlider;
 
+/**
+ *  Mehtod used to build the slider with selected value
+ *
+ *  @param expected value slider value
+ */
 - (void)buildSliderWithValue:(NSInteger) value;
 
+/**
+ *  Return the actual slider value
+ *
+ *  @return actual slider value
+ */
 - (NSInteger)getSliderValue;
 
+/**
+ *  Set the slider value
+ *
+ *  @param value expected slider value
+ */
 - (void)setSliderValue:(NSInteger) value;
 
+/**
+ *  Set the slider color
+ *
+ *  @param sliderColor expected slider color
+ */
 - (void)setSliderColor:(UIColor *)sliderColor;
 
+/**
+ *  Set the slider text color
+ *
+ *  @param textColor expected slider text color
+ */
 - (void)setTextColor:(UIColor *)textColor;
 
 @end
