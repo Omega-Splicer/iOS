0486e5ce2b4446cbcaf27004f625d4d08f4907b2
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-04 16:34:56 +0300

    OSFlyPortraitViewController unit tests

---
+++ Omega-Splicer/Omega-SplicerTests/OSFlyPortraitViewControllerTests.m
@@ -0,0 +1,33 @@
+//
+//  OSFlyPortraitViewControllerTests.m
+//  Omega-Splicer
+//
+//  Created by Charles-Adrien Fournier on 04/04/16.
+//  Copyright © 2016 Charles-Adrien Fournier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "OSFlyPortraitViewController.h"
+
+@interface OSFlyPortraitViewControllerTests : XCTestCase
+
+@property (strong, nonatomic) OSFlyPortraitViewController *flyPortraitViewController;
+
+@end
+
+@implementation OSFlyPortraitViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    self.flyPortraitViewController = [[OSFlyPortraitViewController alloc] init];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testFlyPortraitViewController {
+    XCTAssertNotNil(self.flyPortraitViewController, @"Fly portrait view controller is nil");
+}
+
+@end
