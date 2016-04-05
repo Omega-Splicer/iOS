cab3d3b62bc1d925b4e7a3761a3da07665fcee7e
Charles-Adrien Fournier <Charladfr@icloud.com>
2016-04-05 22:33:31 +0300

    Complete unit test for the model OSPlane

--- Omega-Splicer/Omega-SplicerTests/Model Tests/OSPlaneTests.m
+++ Omega-Splicer/Omega-SplicerTests/Model Tests/OSPlaneTests.m
@@ -13,7 +13,15 @@
 
 @property (strong, nonatomic) OSPlane *examplePlane;
 
+@property (strong, nonatomic) OSPlane *examplePlane2;
+
+@property (strong, nonatomic) OSPlane *examplePlane3;
+
 @property (strong, nonatomic) OSPlane *customPlane;
+
+@property (strong, nonatomic) OSPlane *customPlane2;
+
+@property (strong, nonatomic) OSPlane *emptyPlane;
 
 @end
 
@@ -24,12 +32,26 @@
 
     self.examplePlane = [[OSPlane alloc] init];
     [self.examplePlane planeExample1];
+
+    self.examplePlane2 = [[OSPlane alloc] init];
+    [self.examplePlane2 planeExample2];
+
+    self.examplePlane3 = [[OSPlane alloc] init];
+    [self.examplePlane3 planeExample3];
+
     
     self.customPlane = [[OSPlane alloc] init];
     [self.customPlane setName:@"MyCustomPlane"];
     [self.customPlane setBattery:95];
     [self.customPlane setSignal:50];
     [self.customPlane setMotor:25];
+    
+    self.customPlane2 = [[OSPlane alloc] init];
+    [self.customPlane2 updatePlaneWithName:@"CustomPlane" battery:24 signal:90 motor:0];
+    [self.customPlane2 setMotor:0];
+    
+    self.emptyPlane = [[OSPlane alloc] init];
+    [self.emptyPlane emptyPlane];
 }
 
 - (void)tearDown {
@@ -44,12 +66,45 @@
     XCTAssertEqual(self.examplePlane.motor, 100);
 }
 
+- (void)testExamplePlane2 {
+    XCTAssertNotNil(self.examplePlane2, @"Example plane 2 is nil");
+    XCTAssertEqualObjects(self.examplePlane2.name, @"Second example plane");
+    XCTAssertEqual(self.examplePlane2.battery, 50);
+    XCTAssertEqual(self.examplePlane2.signal, 50);
+    XCTAssertEqual(self.examplePlane2.motor, 50);
+}
+
+- (void)testExamplePlane3 {
+    XCTAssertNotNil(self.examplePlane3, @"Example plane 3 is nil");
+    XCTAssertEqualObjects(self.examplePlane3.name, @"Third example plane");
+    XCTAssertEqual(self.examplePlane3.battery, 0);
+    XCTAssertEqual(self.examplePlane3.signal, 0);
+    XCTAssertEqual(self.examplePlane3.motor, 0);
+}
+
 - (void)testCustomPlane {
     XCTAssertNotNil(self.customPlane, @"Custom plane is nil");
     XCTAssertEqualObjects(self.customPlane.name, @"MyCustomPlane");
     XCTAssertEqual(self.customPlane.battery, 95);
     XCTAssertEqual(self.customPlane.signal, 50);
     XCTAssertEqual(self.customPlane.motor, 25);
+}
+
+- (void)testCustomPlane2 {
+    XCTAssertNotNil(self.customPlane, @"Custom plane is nil");
+    XCTAssertEqualObjects(self.customPlane2.name, @"CustomPlane");
+    XCTAssertEqual(self.customPlane2.battery, 24);
+    XCTAssertEqual(self.customPlane2.signal, 90);
+    XCTAssertEqual(self.customPlane2.motor, 0);
+}
+
+- (void)testEmptyPlane {
+    XCTAssertNotNil(self.emptyPlane, @"Example plane is nil");
+    XCTAssertEqualObjects(self.emptyPlane.name, @"");
+    XCTAssertEqual(self.emptyPlane.battery, 0);
+    XCTAssertEqual(self.emptyPlane.signal, 0);
+    XCTAssertEqual(self.emptyPlane.motor, 0);
+
 }
 
 @end
